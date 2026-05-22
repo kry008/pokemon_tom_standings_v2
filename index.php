4a224dac1f9c890bce6cfb127d8d471f8e66a80d
kry008 <git@mail.kry008.xyz>
2026-05-22 12:15:15 +0200

    Base URL

--- index.php
+++ index.php
@@ -224,7 +224,7 @@ function generateHTML($tournamentName, $pods, $playerMap): string {
     </div>
 </header>
 <main>
-    <h2 style="font-size:1.4em; color:#555;"><?php echo getBaseUrl(); ?></h2>
+    <h2 style="font-size:1.4em; color:#555;">{$url}</h2>
     {$sections}
 </main>
 </body>
